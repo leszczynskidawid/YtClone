b78a30935340ae762e74ab2a952dab1f453f4613
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2023-02-23 16:25:37 +0100

    eslint and prettier setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const handle = () => {
+    console.log("Sdasd");
+  };
+  return (
+    <div className="App">
+      <div>ja pierodle</div>
+
+      <h1>sdasdasd</h1>
+
+      <button onClick={() => handle()} />
+    </div>
+  );
+}
+
+export default App;
